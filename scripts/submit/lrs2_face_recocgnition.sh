9f65ee11e7a3deafaf8f7eacaef05f741a6d5efd
zero0kiriyu <517855956@qq.com>
2023-02-07 23:33:56 +0800

    add script

---
+++ scripts/submit/lrs2_face_recocgnition.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=lrs2_face_recognition
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+#SBATCH --comment=WHMFC_news
+
+# activate conda env
+source /group_homes/public_cluster/home/share/LipReadingGroup/enviroment/.bashrc
+env
+nvcc --version
+nvidia-smi
+which conda
+which python
+conda activate cuda11.7-torch1.13
+cd ~/share/LipReadingGroup/lipreading
+which python
+python -u preprocess/lrs2_face_recognition.py
