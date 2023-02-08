8a2a307cbb6a572357cbccd71d2fdb9cdfea13d3
zero0kiriyu <517855956@qq.com>
2023-02-08 14:09:29 +0800

    update tracking notebook

--- lipreading/utils/face_tracker/bot_sort.py
+++ lipreading/utils/face_tracker/bot_sort.py
@@ -229,12 +229,11 @@ class BoTSORT(object):
         self.proximity_thresh = proximity_thresh
         self.appearance_thresh = appearance_thresh
         
-        self.encoder = reid_model
         if reid_model:
             self.with_reid = True
+            self.encoder = reid_model
         else:
             self.with_reid = False
-
         self.gmc = GMC()
 
     def update(self, bboxes,kps, img):
@@ -373,7 +372,6 @@ class BoTSORT(object):
         
         if self.with_reid:
             emb_dists = matching.embedding_distance(unconfirmed, detections) / 2.0
-            raw_emb_dists = emb_dists.copy()
             emb_dists[emb_dists > self.appearance_thresh] = 1.0
             emb_dists[ious_dists_mask] = 1.0
             dists = np.minimum(ious_dists, emb_dists)
